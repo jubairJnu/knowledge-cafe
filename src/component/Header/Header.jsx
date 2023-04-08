fc7171a1aba3e2cf398538a8e4aecca6c6ba8db3
jubairJnu <jubair2810@gmail.com>
2023-04-08 21:49:44 +0600

    nav-bar-added

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
   return (
+    <nav className='navbar'>
+      
     <div>
       <h2>Knowledge Blog</h2>
     </div>
+    
+    <div className='nav-style'>
+
+    <a href="/home">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    </div>
+    
+    </nav>
   );
 };
 
